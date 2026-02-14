6f9fc3edaf49c55e7c20dec516ce9dbabc106866
Devendra Singh Thakur <imdevendra363@gmail.com>
2026-02-14 21:55:08 +0530

    Armstrong number code is added

--- armstrong.py
+++ armstrong.py
@@ -19,3 +19,8 @@ for i in range(1,1001): #to print all armstrong numbers from 1 to 1000
         print(f"{i} is armstrong")
         count +=1
 print(count) #prints number of armstrong numbers
+
+# Complexity Analysis
+# Time Complexity: O(log10N + 1) where N is the input number. The time complexity is determined by the number of digits in the input integer N. In the worst case when N is a multiple of 10 the number of digits in N is log10 N + 1.
+
+# Space Complexity: O(1) as only a constant amount of additional memory for the reversed number regardless of size of the input number.
