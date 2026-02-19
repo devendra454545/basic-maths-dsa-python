948df3de47175f06c589f75b45194916e442c007
Devendra Singh Thakur <imdevendra363@gmail.com>
2026-02-19 23:53:36 +0530

    GCD code added

--- gcd.py
+++ gcd.py
@@ -8,3 +8,7 @@ def gcd(n1, n2):
     return 1
 
 print(gcd(12, 8)) 
+
+# Complexity Analysis:
+# Time Complexity: O(min(N1, N2)) where N1 and N2 is the input number. The algorithm iterates from the minimum of N1 and N2 to 1 and each iteration checks whether both the numbers are divisible by the current number (constant time operations).
+# Space Complexity: O(1) as the space complexity remains constant and independent of the input size. Only a fixed amount of memory is required to store the integer variables.
